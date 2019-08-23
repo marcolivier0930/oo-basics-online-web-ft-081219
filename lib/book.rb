8af302315fcceca87ba73f44b100cc2640c47182
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-23 06:17:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,15 @@
 class Book
-  def initialize(title, author, page_count, genre)
+  def initialize(title)
     @title = title
   end
   def title
     @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  def author
+    @author
   end
 end
